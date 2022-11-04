a021e0b7d6b473c8c54fd6d57dc119bf8af636ab
iqfareez <iqmal3@outlook.com>
2022-11-04 20:14:19 +0800

    :pencil2: Edit to optimize code
    https://forum.arduino.cc/t/declaring-array-of-objects/1047144/5?u=fareezmewo

--- Week4/ex_9/ex_9.ino
+++ Week4/ex_9/ex_9.ino
@@ -1,75 +1,73 @@
 /*
-There are two LEDs whose anodes are connected to PD5 and PD6 respectively. The LED 1
-needs to be flashing with ON time 750 ms and OFF time 350 ms. The LED 2 needs to be
-flashing with ON time 400 and OFF time 600 ms
-*/
-
+ * There are two LEDs whose anodes are connected to PD5 and PD6 respectively. The LED 1
+ * needs to be flashing with ON time 750 ms and OFF time 350 ms. The LED 2 needs to be
+ * flashing with ON time 400 and OFF time 600 ms
+ */
 
 /**
- * Blink LED with given port and pin with certain
- * amount of on duration and off duration
- * 
- * Supported port: B, C, D
- * Supported pin: 1-7
- *
- */
+   Blink LED with given port and pin with certain
+   amount of on duration and off duration
+
+   Supported port: B, C, D
+   Supported pin: 1-7
+
+*/
 class LedBlinky {
-  char port;
-  int pin;
-  int pinBin;
-  int onDurationMs;
-  int offDurationMs;
-  unsigned char *writer;  // Port data register
-  unsigned long previousMillis;
+    char port;
+    int pin;
+    int pinBin;
+    int onDurationMs;
+    int offDurationMs;
+    unsigned char *writer;  // Port data register
+    unsigned long previousMillis;
 
+  public:
+    LedBlinky(char port, int pin, int onDur, int offDur) {
+      port = port;
+      pin = pin;
+      onDurationMs = onDur;
+      offDurationMs = offDur;
 
-public:
-  LedBlinky(char port, int pin, int onDur, int offDur) {
-    port = port;
-    pin = pin;
-    onDurationMs = onDur;
-    offDurationMs = offDur;
+      previousMillis = 0;
 
-    previousMillis = 0;
-
-    // hardcode the binary representation for now
-    switch (pin) {
-      case 0: pinBin = 0b00000001; break;
-      case 1: pinBin = 0b00000010; break;
-      case 2: pinBin = 0b00000100; break;
-      case 3: pinBin = 0b00001000; break;
-      case 4: pinBin = 0b00010000; break;
-      case 5: pinBin = 0b00100000; break;
-      case 6: pinBin = 0b01000000; break;
-      case 7: pinBin = 0b10000000; break;
-    }
-
-    switch (port) {
-      case 'B': writer = (unsigned char *)0x25; break;
-      case 'C': writer = (unsigned char *)0x28; break;
-      case 'D': writer = (unsigned char *)0x2b; break;
-    }
-  }
-  void Run() {
-    unsigned long currentTime = millis();
-
-    if (((*writer) & pinBin) == 0)  //If the LED is currently OFF
-    {
-      if (currentTime - previousMillis >= offDurationMs)  // OFF duration
-      {
-        *writer |= pinBin;  // Turn ON LED
-        previousMillis = currentTime;
+      // hardcode the binary representation for now
+      switch (pin) {
+        case 0: pinBin = 0b00000001; break;
+        case 1: pinBin = 0b00000010; break;
+        case 2: pinBin = 0b00000100; break;
+        case 3: pinBin = 0b00001000; break;
+        case 4: pinBin = 0b00010000; break;
+        case 5: pinBin = 0b00100000; break;
+        case 6: pinBin = 0b01000000; break;
+        case 7: pinBin = 0b10000000; break;
       }
-    } else  //LED is currently ON
-    {
-      if (currentTime - previousMillis >= onDurationMs)  // ON duration
-      {
-        // Bitwise, if pinBin is 0b00001000, then ~pinBin is 0b11110111
-        *writer &= ~pinBin;  // Turn OFF Led.
-        previousMillis = currentTime;
+
+      switch (port) {
+        case 'B': writer = (unsigned char *)0x25; break;
+        case 'C': writer = (unsigned char *)0x28; break;
+        case 'D': writer = (unsigned char *)0x2b; break;
       }
     }
-  }
+    void Run() {
+      unsigned long currentTime = millis();
+
+      if (((*writer) & pinBin) == 0)  //If the LED is currently OFF
+      {
+        if (currentTime - previousMillis >= offDurationMs)  // OFF duration
+        {
+          *writer |= pinBin;  // Turn ON LED
+          previousMillis = currentTime;
+        }
+      } else  //LED is currently ON
+      {
+        if (currentTime - previousMillis >= onDurationMs)  // ON duration
+        {
+          // Bitwise, if pinBin is 0b00001000, then ~pinBin is 0b11110111
+          *writer &= ~pinBin;  // Turn OFF Led.
+          previousMillis = currentTime;
+        }
+      }
+    }
 };
 
 LedBlinky leds[] = {
@@ -83,14 +81,12 @@ LedBlinky leds[] = {
   LedBlinky('B', 1, 777, 888),
 };
 
-int numOfLeds = sizeof(leds)/sizeof(int);
+const byte numOfLeds = sizeof leds / sizeof * leds;
 
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
-  for (int i = 0; i < numOfLeds; i++) {
-    leds[i].Run();
-  }
+  for (auto& aLed : leds) aLed.Run(); // https://forum.arduino.cc/t/declaring-array-of-objects/1047144/5?u=fareezmewo
 }
